250101e446b2a7ac3f6968df2848f63b293c784b
Minarets-Create <143848628+Minarets-Create@users.noreply.github.com>
2023-09-05 17:59:37 -0700

    Update main_array.py

--- Minarets-Create Python/main_array.py
+++ Minarets-Create Python/main_array.py
@@ -1,22 +1,33 @@
 from PIL import Image
 import tkinter
-from tkinter import turtle
+import turtle
 import math
+t = turtle.Turtle()
 gsize = 100
-ts = 20
+ts = 10
 wx = gsize*ts
 wy = gsize*ts
 wn = turtle.Screen()
+t.hideturtle()
+t.speed(0)
 wn.colormode(255)
 wn.setup(wx,wy)
-t.pensize(tt)
-img = image.new('RGB', (wx,wy), "white")
+img = Image.new('RGB', (wx,wy), "white")
 Canvas_Array = []
+print(gsize**2)
+def draw_square(din):
+    t.setheading(0)
+    t.begin_fill()
+    for i in range(4):
+        t.forward(din)
+        t.right(90)
+    t.end_fill()
 def Canvas_Update():
-  for i in range((gsize**2)/ts):
-    Canvas_Array.clear()
-    color = img.getpixel((i%gsize+ts-1,math.floor(i/gsize)+ts-1)
-    R = color[0]
-    G = color[1]
-    B = color[2]
-    Canvas_Array.append([R,G,B])
+    for i in range((int(gsize**2/ts))):
+        Canvas_Array.clear()
+        color = img.getpixel((i%gsize+ts-1,math.floor(i/gsize)+ts-1))
+        R = color[0]
+        G = color[1]
+        B = color[2]
+        Canvas_Array.append([R,G,B])
+        print([R,G,B])
